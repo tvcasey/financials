33606ae598b2f8fe28ee4a41538245e0bf207d6d
Timothy Casey <tvcasey@yahoo.com>
2021-03-11 20:48:38 -0500

    Messing around with income statement information from Alpha Vantage.  I'd like to move it around, style it, and choose the years, if possible.  I'd like to move the information to parent and child.

--- src/Components/IncomeStatement.jsx
+++ src/Components/IncomeStatement.jsx
@@ -2,42 +2,68 @@
 
 
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'reactstrap';
+import axios from 'axios';
 
-const IncomeStatement = (props) => {
+function IncomeStatement() {
+
+  const [income, setIncome] = useState({annualReports: []});
+
+useEffect(() => {
+  axios.get('https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo')
+    .then(res => {
+      console.log(res.data);
+      setIncome(res.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}, []);
+
+
   return (
-    <Table striped>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>NET INCOME</th>
-          <th>YEAR</th>
-          <th>REVENUES</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>ROMAN</td>
-          <td>GREEK</td>
-          <td>OTTOMAN</td>
-        </tr>
-      </tbody>
+    <Table striped>      
+        <div>            
+              {income.annualReports.map((annualReports) => ( 
+              <div key={annualReports.fiscalEndingDate}>
+        <thead>
+          <tr>
+            <th>INCOME STATEMENT</th>
+            <th>{annualReports.fiscalDateEnding}(billions)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Total Revenue</th>
+            <td>{annualReports.totalRevenue/1000000000}</td>
+          </tr>
+          <tr>
+            <th scope="row">Total Operating Expense</th>
+            <td>{annualReports.totalOperatingExpense/1000000000}</td>
+          </tr>
+          <tr>
+            <th scope="row">Cost of Revenue</th>
+            <td>{annualReports.costOfRevenue/1000000000}</td>
+          </tr>
+          <tr>
+            <th scope="row">Gross Profit</th>
+            <td>{annualReports.grossProfit/1000000000}</td>
+          </tr>
+          <tr>
+            <th scope="row">Ebit</th>
+            <td>{annualReports.ebit/1000000000}</td>
+          </tr>
+          <tr>
+            <th scope="row">Net Income</th>
+            <td>{annualReports.netIncome/1000000000}</td>
+          </tr>
+        </tbody>
+        </div>
+                  ))}
+      </div>
     </Table>
-  );
+        );
 }
 
 export default IncomeStatement;
